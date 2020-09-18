df0e85179f9b693db496de194cb8098abdef762b
joao junior <juniorgtr123@gmail.com>
2020-09-17 21:59:08 -0300

    update appIonic

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ApiService {
+  public url: string = environment.baseUlr;
+
+  constructor(httpClient: HttpClient) {}
+}
